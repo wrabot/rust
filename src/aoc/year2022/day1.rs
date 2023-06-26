48903f2a1e43e99d725ad4e740255d7c85678c5a
Wilfrid Rabot <wilfrid.rabot@orange.com>
2023-06-26 19:32:09 +0200

    Fix year 2022 day 1

--- src/aoc/year2022/day1.rs
+++ src/aoc/year2022/day1.rs
@@ -1,11 +1,20 @@
-pub fn part1(input: &str) -> i32 { calories(input).into_iter().max().unwrap() }
+use itertools::Itertools;
 
-pub fn part2(input: &str) -> i32 { calories(input).into_iter().sum() }
+pub fn part1(input: &str) -> i32 {
+    calories(input).into_iter().max().unwrap()
+}
+
+pub fn part2(input: &str) -> i32 {
+    calories(input).iter().sorted().rev().take(3).sum()
+}
 
 fn calories(input: &str) -> Vec<i32> {
-    return input.split("\n\n").map(|elf|
-        elf.split("\n").map(|snack|
-            snack.parse::<i32>().unwrap()
-        ).sum()
-    ).collect::<Vec<_>>();
+    input
+        .split("\n\n")
+        .map(|elf| {
+            elf.split("\n")
+                .map(|snack| snack.parse::<i32>().unwrap())
+                .sum()
+        })
+        .collect_vec()
 }
